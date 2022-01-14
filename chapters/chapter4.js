f52ab6e33c8a1cf75d7d47181f85e91f54096816
Davina-dev <davinamr91@gmail.com>
2022-01-14 16:18:44 +0100

    Solution The sum of a range

--- chapters/chapter4.js
+++ chapters/chapter4.js
@@ -1,80 +1,91 @@
-//  Sesión 3: arrays
-//Las propiedades que contienen funciones generalmente son llamadas metodos del valor al que pertenecen. Como en, “toUpperCase es un método de string”.
-//Los arrays son un tipo de objeto especializado para almacenar secuencias de cosas. 
+//  Estructuras de Datos: Objetos y Arrays
 
 
-// EJERCICIOS
 
-//ej Suma de un rango******************
+//ej1:La suma de un rango
 
-function rango (inicio, final, optional){
-    //optional == undefined ? console.log('no usas parametro opcional') :console.log(optional + ' es el opcional')  ;
 
-if(optional !=undefined ){
-    arrayNum = [];
-    for(i=inicio; i<=final; i += optional){
-        arrayNum.push(i);
-    } 
-}else{
-    arrayNum = [];
-        for(i=inicio; i<=final; i++){
-            arrayNum.push(i);
-        }
+function rango(inicio, fin, ordenDePaso = inicio <= fin ? 1 : -1) {
+    let resultado = [];
+ 
+    for(let i = inicio; ordenDePaso >= 0 ? i <= fin: i >= fin; i+=ordenDePaso) {
+      resultado.push(i);
     }
-        return arrayNum;
-
-}
-
-
-function sum(arrayNum){  
-    suma = 0;
-    for(var i = 0; i < arrayNum.length; i++){   
-       suma +=arrayNum[i];  
-    }
-       return suma;
-}
-
-
-
-console.log(rango(2, 5));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(sum(rango(1, 10)));
-// → 55 
-
-//funcion rango con parametro opcional ????
-console.log(rango(5, 2, -1));
-// → [5, 4, 3, 2] no funciona!!!!!!!!
-
-
-
-
-
-/*ej Revirtiendo un array *************
-
-
-let arr = [1,2,5,7,8,9];
-
-
-function revertirArray(arr){
-
-
-revertirArray(arr);
-
-
-function revertirArrayEnSuLugar(){
+    return resultado;
+  }
   
-};
+  function suma(numeros) {
+    resultadoSuma = 0;
+    for(let numero of numeros) {
+      resultadoSuma += numero;
+    }
+    return resultadoSuma;
+  }
+  
+  console.log(rango(5, 10));
+  // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  console.log(rango(2, 10, 2));
+  // → [1, 3, 5, 7, 9]
+  console.log(rango(5, 2, -1));
+  // → [5, 4, 3, 2]
+  console.log(suma(rango(1, 10)));
+  // → 55
+
+  
+
+
+//ej Revirtiendo un array *************
+
+
+function revertirArray(array) {
+  resultado = [];
+  for (let item of array) {
+    resultado.unshift(item);
+  }
+  return resultado;
+}
+
+//❌ me cuesta  entender la función ((lengthArray/2)?)
+function revertirArrayEnSuLugar(array) {
+  let lengthArray= array.length;
+  for (let i = 0; i < Math.floor(lengthArray/2); i++) {
+    console.log(i, lengthArray-i-1, array[i], array[lengthArray-i-1], array);
+    let intercambio = array[i];
+    array[i] = array[lengthArray-i-1];
+    array[lengthArray-i-1] = intercambio;
+  }
+  return array;
+}
+
+
+console.log(revertirArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let valorArray = [1, 2, 3, 4, 5];
+
+revertirArrayEnSuLugar(valorArray); //?
+console.log(valorArray);//?
+// → [5, 4, 3, 2, 1]
 
 
 
 
 
-
-
-//ej Una lista *************************
+//❌ ej Una lista / !=array / ==conjunto anidado de objetos, el primer objeto contenie referencia al segundo, el segundo al tercero,etc.
 
 
 
 
 
-//Comparación profunda *****************/
+//❌ Comparación profunda 
+
+function igualdadProfunda(a, b) {
+    if (a === b) {
+      return true;
+    } else if (typeof a === 'object' && a !== null && typeof b === 'object' && b !== null) { //typeof null también produce "object"
+      let keys = Object.keys(a).concat(Object.keys(b)); //Object.keys-> útil para revisar las propiedades de los objetos para compararlos.
+      console.log(keys);
+      }
+}
+  
+  
+console.log(igualdadProfunda(object, object));
